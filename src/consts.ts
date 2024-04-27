1848a12e796f6f17f138bfd70f2245e314e1403c
Vítor Barroso <accounts@owozsh.dev>
2024-04-27 16:16:37 -0300

    Add dir navigation

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Vítor Barroso';
+export const SITE_DESCRIPTION = 'Random description';
