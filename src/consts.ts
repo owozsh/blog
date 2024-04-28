d12a3a67ea1cd4b4ab39a95ecde2f50b19cabfad
Vítor Barroso <accounts@owozsh.dev>
2024-04-28 16:53:47 -0300

    build

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Vítor Barroso";
-export const SITE_DESCRIPTION = "Random description";
+export const SITE_DESCRIPTION = "Vítor Barroso's (aka owozsh) blog";
