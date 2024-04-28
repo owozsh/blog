fc7d554aca29a71014ebb0306135c70b423ad62f
Vítor Barroso <accounts@owozsh.dev>
2024-04-28 17:28:06 -0300

    Remove base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://owozsh.dev",
-  base: "/blog",
   integrations: [
     mdx(),
     sitemap(),
