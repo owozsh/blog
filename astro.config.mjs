6d4473f3c38e15b14434d8720f0b4c63c6b9b8ca
Vítor Barroso <accounts@owozsh.dev>
2024-05-01 15:46:35 -0300

    Add new post

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,26 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://owozsh.dev",
+  markdown: {
+    shikiConfig: {
+      // theme: "solarized-dark",
+      // Alternatively, provide multiple themes
+      // https://shiki.style/guide/dual-themes
+      themes: {
+        light: "rose-pine-dawn",
+        dark: "rose-pine-moon",
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
   integrations: [
     mdx(),
     sitemap(),
